663dab419f0cd70e2aab0c2018006e3329a13476
cyberonites <cyberonitesclub@gla.ac.in>
2024-06-19 08:19:06 +0530

    Web Testing

--- src/components/MobileBlock.jsx
+++ src/components/MobileBlock.jsx
@@ -1,24 +1,26 @@
 import React from "react";
+import MobileBlock from "../img/MobileBlock.png";
+import "../css/component-css/mobileblock.css";
 
 const Optimizing = () => {
   return (
-    <div style={styles.container}>
-      <h1>We are optimizing our website for mobile and tablet users.</h1>
-      <p>Please check back later.</p>
+    <div className="block-mobile">
+      <img
+        src={MobileBlock}
+        alt="Optimizing For Mobiles and Tablets"
+        className="block-image"
+      />
+      <p className="reason">
+        We Are Optimising Our Website for mobile and tablet users.
+      </p>
+      <p className="apolo">
+        For any support reach us at{" "}
+        <a href="mailto:support@cyberonites.com" className="support-channel">
+          Support Channel.
+        </a>
+      </p>
     </div>
   );
-};
-
-const styles = {
-  container: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100vh",
-    textAlign: "center",
-    backgroundColor: "#f0f0f0",
-    padding: "0 20px",
-  },
 };
 
 export default Optimizing;
